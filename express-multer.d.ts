34f2de2e2d14b488fdfef712718718bc09132992
Ederson Vincius <evfr.trabalho@gmail.com>
2025-01-29 16:15:04 -0300

    Update express-multer.d.ts

--- express-multer.d.ts
+++ express-multer.d.ts
@@ -1,10 +1,9 @@
-// express-multer.d.ts
 import { Request } from 'express';
 import { File } from 'multer';
 
 declare module 'express-serve-static-core' {
   interface Request {
-    file?: File; // Para lidar com um único arquivo
-    files?: File[]; // Caso você queira suportar múltiplos arquivos no futuro
+    file?: File;
+    files?: File[];
   }
 }
