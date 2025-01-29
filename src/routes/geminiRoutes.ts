aec0ef445e768913ebb29cebfbf818089d612f9f
Ederson Vincius <evfr.trabalho@gmail.com>
2025-01-29 16:13:07 -0300

    Update geminiRoutes.ts

--- src/routes/geminiRoutes.ts
+++ src/routes/geminiRoutes.ts
@@ -5,7 +5,6 @@ import fs from 'fs';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
-// Inicializar GoogleGenerativeAI
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const fileManager = new GoogleAIFileManager(process.env.API_KEY);
 const geminiRouter = Router();
@@ -18,7 +17,6 @@ geminiRouter.post('/', async (request: Request, response: Response) => {
       return response.status(400).send("Imagem não fornecida.");
     }
 
-    // Verificar e remover o prefixo "data:image/png;base64,"
     const base64Pattern = /^data:image\/(jpeg|png|gif);base64,/;
     const matches = image.match(base64Pattern);
 
@@ -33,24 +31,19 @@ geminiRouter.post('/', async (request: Request, response: Response) => {
     const tempFileName = `${uuidv4()}.${mimeType}`;
     const tempFilePath = path.join(__dirname, tempFileName);
 
-    // Escrever os dados base64 no arquivo temporário
     fs.writeFileSync(tempFilePath, base64Data, 'base64');
 
-    // Fazer o upload do arquivo temporário usando o caminho do arquivo
     const uploadResponse = await fileManager.uploadFile(tempFilePath, {
       mimeType: `image/${mimeType}`,
       displayName: "Jetpack drawing",
     });
 
-    // Remover o arquivo temporário após o upload
     fs.unlinkSync(tempFilePath);
 
-    // Inicializar o modelo Gemini
     const model = genAI.getGenerativeModel({
       model: "gemini-1.5-Flash",
     });
 
-    // Gerar conteúdo usando o modelo Gemini
     const result = await model.generateContent([
       {
         fileData: {
@@ -61,10 +54,8 @@ geminiRouter.post('/', async (request: Request, response: Response) => {
       { text: "Describe this image." },
     ]);
 
-    // Output the generated text to the console
     console.log(result.response.text());
 
-    // Responder ao cliente com sucesso e com o valor gerado
     response.status(200).send({
       message: "Arquivo enviado e conteúdo gerado com sucesso",
       fileUri: uploadResponse.file.uri,
